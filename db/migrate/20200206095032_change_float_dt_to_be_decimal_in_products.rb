af7e64faccc2f297925cb68aa28cf79a8cf564c9
tesdeve <tesdeve@gmail.com>
2020-02-14 17:50:27 +0000

    Ajax sorted

--- db/migrate/20200206095032_change_float_dt_to_be_decimal_in_products.rb
+++ db/migrate/20200206095032_change_float_dt_to_be_decimal_in_products.rb
@@ -1,5 +1,10 @@
 class ChangeFloatDtToBeDecimalInProducts < ActiveRecord::Migration[6.0]
-  def change
+  def up
     change_column :products, :valor_cop, :decimal
   end
+
+  def down
+    change_column :products, :valor_cop, :float
+  end
+
 end
