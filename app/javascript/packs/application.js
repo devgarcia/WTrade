107e4f87bb924ff75bb8fedf77494bf45a812575
tesdeve <tesdeve@gmail.com>
2020-02-18 15:37:15 +0000

    PWA install button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,20 +31,3 @@ if (navigator.serviceWorker) {
 }
 
 
-//import 'jquery'
-//import 'autonumeric'
-// require 'jquery'
-// require'autonumeric'
-
-// $(document).ready(function(){
-	// $('#valor_maximo_cop').mask('000.000.000.000.000,00', {reverse: true});
-// });
-
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
